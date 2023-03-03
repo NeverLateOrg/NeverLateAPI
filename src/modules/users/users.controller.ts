781d4ab6deee2581ec390fd8e67e8426aaf94ed3
Mike Li <mike.li@epita.fr>
2023-03-03 20:42:57 +0100

    chore:  create base file architecture

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async addUser(): Promise<string> {
+    const result = await this.usersService.addUser('Pierre', 'pierre@gmail.com');
+    return result;
+  }
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { type User } from './user.model';
+import { User } from './schemas/user.schema';
 
 @Injectable()
-export class UserService {
+export class UsersService {
   constructor(@InjectModel('User') private readonly UserModel: Model<User>) {}
 
   async addUser(name: string, email: string): Promise<string> {
