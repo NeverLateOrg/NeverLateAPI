443f94b2d02b25626c56c2d5411ba1b3eeb1b2ef
Adrien Pingard <adrien.pingard@gmail.com>
2023-03-05 16:55:33 +0100

    test(e2e and units): removes defaults tests and allow no tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { type INestApplication } from '@nestjs/common';
 import { Test, type TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -15,7 +14,7 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', async () => {
-    return await request(app.getHttpServer()).get('/').expect(200).expect('Hello World!!');
+  it('empty', async () => {
+    // return await request(app.getHttpServer()).get('/').expect(200).expect('Hello World!!');
   });
 });
