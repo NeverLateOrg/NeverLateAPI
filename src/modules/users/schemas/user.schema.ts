86fb5ddab311eb91988c8316bbab9f3bcd69f288
Mike Li <mike.li@epita.fr>
2023-03-26 18:34:32 +0200

    feat(user module): user schema with decorator syntax and events field
    - add the events field that is a field of ObjectId with a ref to the Event schema
    - improve syntax with decorators

--- src/modules/users/schemas/user.schema.ts
+++ src/modules/users/schemas/user.schema.ts
@@ -1,12 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
+import { Event } from '../../events/schemas/event.schema';
 
-export const UserSchema = new mongoose.Schema({
-  email: String,
-  name: String,
-});
+export type UserDocument = User & mongoose.Document;
 
-export interface User {
-  id: string;
+@Schema()
+export class User {
+  @Prop({ type: String, required: true })
   email: string;
+
+  @Prop({ type: String, required: true })
   name: string;
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: Event }], required: true })
+  events: string[];
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
