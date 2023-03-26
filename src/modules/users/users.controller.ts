12cf414ba5e5a8a8b84bc955ccabed5a17815bc0
Julien Gourmet <julien.gourmet@epita.fr>
2023-03-26 22:35:41 +0200

    feat(user.service / user.controller / package,json): modif creation user / login
    hachage du mot de passe lors de la creation, check si user pas deja present, fonction de login
    fonctionnelle
    
    BREAKING CHANGE: package.json

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import {Body, Controller, Get, HttpException, HttpStatus, Post, Res} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -7,10 +7,13 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async addUser(@Body() user: any, @Res() res): Promise<void> {
-    const result = await this.usersService.addUser(user.name, user.email, user.password);
-    res.status(200).send(result);
-    // return result;
+  async createUser(@Body() user: any, @Res() res): Promise<void> {
+    const result = await this.usersService.createUser(user.firstname, user.lastname, user.email, user.password);
+
+    if (result.includes("db"))
+      res.status(409).send(result);
+    else
+      res.status(201).send(result);
   }
 
   @Get()
@@ -18,5 +21,14 @@ export class UsersController {
     const result = await this.usersService.getUser(user.email);
     if (result.includes('not found')) res.status(404).send(result);
     res.status(200).send(result);
+  }
+
+  @Post('login')
+  async loginUser(@Body() body: { email: string, password: string }): Promise<{ message: string; user: any }> {
+    const user = await this.usersService.loginUser(body.email, body.password);
+    if (user == null) {
+      throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+    }
+    return { message: 'Logged in successfully', user };
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,13 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel('User') private readonly UserModel: Model<User>) {}
 
-  async addUser(name: string, email: string, password: string): Promise<string> {
-    const user = new this.UserModel({ name, email, password });
+  async createUser(firstname: string, lastname: string, email: string, password: string): Promise<string> {
+    let found = await this.getUser(email);
+    found = String(found);
+    if (!found.includes('not found')) {
+      return 'user already present in db';
+    }
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const user = new this.UserModel({ firstname, lastname, email, password: hashedPassword });
     await user.save();
     return 'new user created';
   }
@@ -17,5 +25,15 @@ export class UsersService {
     const user = await this.UserModel.findOne({ email }).exec();
     if (user != null) return user.email;
     return 'user `' + email + '` not found';
+  }
+
+  async loginUser(email: string, password: string): Promise<User | null> {
+    const user = await this.UserModel.findOne({ email }).exec();
+    if (user == null) return null;
+
+    const isPasswordMatching = await bcrypt.compare(password, user.password);
+    if (isPasswordMatching === false) return null;
+
+    return user;
   }
 }
