44e5ff7a8c440c2ebf51283d2b9f3cea85a8edb3
Julien Gourmet <julien.gourmet@epita.fr>
2023-03-27 10:48:49 +0200

    refactor(controller.ts): add file for environnement var

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { Body, Controller, Get, HttpException, HttpStatus, Post, Res } from '@nestjs/common';
 import { UsersService } from './users.service';
+import * as jwt from 'jsonwebtoken';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+const secretKey = process.env.JTW_SECRET_KEY;
 
 @Controller('users')
 export class UsersController {
@@ -22,11 +27,12 @@ export class UsersController {
   }
 
   @Post('login')
-  async loginUser(@Body() body: { email: string; password: string }): Promise<{ message: string; user: any }> {
+  async loginUser(
+    @Body() body: { email: string; password: string },
+  ): Promise<{ message: string; user: any; token: any }> {
     const user = await this.usersService.loginUser(body.email, body.password);
-    if (user == null) {
-      throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
-    }
-    return { message: 'Logged in successfully', user };
+    if (user == null) throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+    const token = jwt.sign({ email: user.email }, secretKey, { expiresIn: '48h' });
+    return { message: 'Logged in successfully', user, token };
   }
 }
