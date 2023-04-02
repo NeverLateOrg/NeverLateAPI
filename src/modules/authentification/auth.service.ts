1e2f820d1d4aafb92df23996246acb1dcdec9e35
Julien Gourmet <julien.gourmet@epita.fr>
2023-04-02 22:31:16 +0200

    refactor: creation of user in srrvice

--- src/modules/authentification/auth.service.ts
+++ src/modules/authentification/auth.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { TokenDTO } from './dtos/token.dto';
+import { User } from '../users/user.schema';
+import * as argon from 'argon2';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
-@Injectable()
-export class AuthService {}
+@Injectable({})
+export class AuthService {
+  constructor(@InjectModel('User') private readonly UserModel: Model<User>) {}
+
+  async register(RegisterDTO): Promise<TokenDTO> {
+    const hashedPassword = argon.hash(RegisterDTO.password);
+
+    const user = new this.UserModel({
+      email: RegisterDTO.email,
+      firstname: RegisterDTO.firstname,
+      lastname: RegisterDTO.lastname,
+      passwordHash: hashedPassword,
+    });
+
+    // save user in db.
+    await user.save();
+    // return jwt token
+    return new TokenDTO();
+  }
+
+  async login(LoginDTO): Promise<TokenDTO> {
+    return new TokenDTO();
+  }
+}
