c2f3d456ee5f536cb7b328eaaaa2fd9aaf93476c
Adrien Pingard <adrien.pingard@gmail.com>
2023-04-17 18:08:06 +0200

    feat(plug location format): plug the google location formator

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LocationValidatorModule } from '../travels/locationFormator/formator.module';
 import { EventSchema } from './event.schema';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }]), LocationValidatorModule],
   providers: [EventsService],
   controllers: [EventsController],
   exports: [EventsService],
